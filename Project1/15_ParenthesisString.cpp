a03156d2ea0973c23180a725d4b1768d9bf975bd
paradox <get_ant@naver.com>
2023-02-20 18:32:17 +0900

    0220

---
+++ Project1/15_ParenthesisString.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    vector<string> vec;
+
+    for (int i = 0; i < num; i++) {
+        string str;
+        cin >> str;
+        bool chk = true;
+        stack<char> stack;
+        for (int j = 0; j < str.size(); j++) {
+            if (str[j] == '(') {
+                stack.push(str[j]);
+            }
+            else if (str[j] == ')') {
+                //-1 error occurs
+                if (stack.size() == 0) {
+                    chk = false;
+                    break;
+                }
+                else {
+                    stack.pop();
+                }
+            }
+            else {
+                break;
+            }
+        }
+        if (stack.size() >= 1) {
+            vec.push_back("NO");
+        }
+        else {
+            if (chk) {
+                vec.push_back("YES");
+            }
+            else {
+                vec.push_back("NO");
+            }
+        }
+    }
+
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << '\n';
+    }
+
+    return 0;
+}
---
+++ Project1/16_Zero.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int count;
+    cin >> count;
+
+    stack<int> stack;
+
+    for (int i = 0; i < count; i++) {
+        int temp;
+        cin >> temp;
+        if (temp == 0) {
+            stack.pop();
+        }
+        else {
+            stack.push(temp);
+        }
+    }
+
+    int sum = 0, sCount = stack.size();
+    for (int i = 0; i < sCount; i++) {
+        sum += stack.top();
+        stack.pop();
+    }
+    cout << sum;
+
+    return 0;
+}
